0071b6748252b12f308cb623fcd151ea993cf42b
Ghislain H <telitos@gmail.com>
2016-11-30 20:32:02 +0000

    no changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,23 @@ class App extends Component {
 
   constructor(props) {
 
-  const data = {
-    currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-    messages: [
-      { id: "1",
-        username: "Bob",
-        content: "Has anyone seen my marbles?",
-      },
-      { id: "2",
-        username: "Anonymous",
-        content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-      }
-    ],
-    currentId: 3
-  };
+
+    const data =
+
+  // const data = {
+  //   currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  //   messages: [
+  //     { id: "1",
+  //       username: "Bob",
+  //       content: "Has anyone seen my marbles?",
+  //     },
+  //     { id: "2",
+  //       username: "Anonymous",
+  //       content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+  //     }
+  //   ],
+  //   currentId: 3
+  // };
 
     super(props)
 
